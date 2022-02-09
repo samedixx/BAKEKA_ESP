2f33e1fb0d6e2441336c109bd2add99199e8de66
samedisonof <samedibenassi@gmail.com>
2022-02-09 19:18:45 +0100

    Start to build API service

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 require('dotenv').config();
 
+const postRouter = require('./routers/post')
+
 const PORT = process.env.PORT;
 
 const app = express();
+
+app.use('/api/post', postRouter)
 
 app.listen(PORT, () => {
     console.log('Express is listening on port -->' + PORT)
---
+++ backend/routers/post.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+
+router.post('/create')
+
+
+module.exports = router
